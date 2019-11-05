389165c10957da9b4045a0f1d0afcf66f27f9e93
yui-wong <micah.wong1121@gmail.com>
2019-11-05 12:58:28 -0500

    -Added a profile and the ability to edit it -Fixed bug with password not saving, will aim to save name and age parameter for users after

--- study_buddy/app/models/user.rb
+++ study_buddy/app/models/user.rb
@@ -8,14 +8,14 @@ class User < ApplicationRecord
     has_and_belongs_to_many :repos
     has_and_belongs_to_many :courses
 
-    attr_accessor :password
+    # attr_accessor :password //This will prevent the password from being saved
 
     # validates :name, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
     # validates :age, :numericality => { :only_integer => true }
     # validates :age, numericality: { greater_than: 15, less_than: 100 }
-    validates :email, :presence => true, :uniqueness => true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-    validates :password, :confirmation => true #password_confirmation attr   #this is for confirming the password 
-    validates_length_of :password, :in => 6..20, :on => :create
+    # validates :email, :presence => true, :uniqueness => true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    # validates :password, :confirmation => true #password_confirmation attr   #this is for confirming the password 
+    # validates_length_of :password, :in => 6..20, :on => :create
 
     # before_save :encrypt_password
     # after_save :clear_password
--- study_buddy/config/routes.rb
+++ study_buddy/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/profile'
   root to: 'pages#home'
   get 'pages/about'
   devise_for :users
