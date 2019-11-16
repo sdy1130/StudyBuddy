e9b9308185ebbb6f1943f586a5110d3688a89e2b
Charlie Yang <seung.yang@mail.utoronto.ca>
2019-11-16 05:07:48 -0500

    changing repos_users to lower case

--- db/migrate/20191106220012_create_join_table_user_repo.rb
+++ db/migrate/20191106220012_create_join_table_user_repo.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableUserRepo < ActiveRecord::Migration[6.0]
   def change
-    create_join_table :Repos, :Users do |t|
+    create_join_table :repos, :users do |t|
       t.index [:repo_id, :user_id]
       t.index [:user_id, :repo_id]
     end
