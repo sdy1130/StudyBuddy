c109cef127acf9958a001fe1ba92f9408c7790b8
Charlie Yang <seung.yang@mail.utoronto.ca>
2019-11-16 03:52:04 -0500

    adding bootstrap-sass gem

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,20 +2,14 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
 
-// Get a pre-configured plugin
-const manifestPlugin = environment.plugins.get('Manifest')
-manifestPlugin.options.writeToFileEmit = false
-
-// Add an additional plugin of your choosing : ProvidePlugin
-environment.plugins.prepend(
-  'Provide',
-  new CaseSensitivePathsPlugin(),
-  new webpack.ProvidePlugin({
+environment.plugins.append('Provide', 
+    new CaseSensitivePathsPlugin(),
+    new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
-    jquery: 'jquery',
-    Popper: ['popper.js', 'default'],
-  })
-)
+    Popper: ['popper.js', 'default']
+}))
+
+module.exports = environment
 
 module.exports = environment
