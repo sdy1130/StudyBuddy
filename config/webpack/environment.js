719e8568ec5f76caa24025c0a959d76e89f11a30
Charlie Yang <seung.yang@mail.utoronto.ca>
2019-11-16 03:17:00 -0500

    adding case sensitive webpacker plugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,25 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+
+// Get a pre-configured plugin
+const manifestPlugin = environment.plugins.get('Manifest')
+manifestPlugin.options.writeToFileEmit = false
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  'Provide',
+  new CaseSensitivePathsPlugin(),
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    'window.Tether': 'tether',
+    Popper: ['popper.js', 'default'],
+    ActionCable: 'actioncable',
+    Vue: 'vue',
+    VueResource: 'vue-resource',
+  })
+)
 
 module.exports = environment
