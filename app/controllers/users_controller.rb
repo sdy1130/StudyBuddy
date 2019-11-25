d7297b402afc06c0151f9a9ea66d2af2c1c6aea0
yui-wong <yui.wong@mail.utoronto.ca>
2019-11-25 12:30:11 -0500

    Now users can view profile without logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
 
   def profile
-    if (params[:format])
+    if (params.has_key?(:format))
       @user = User.find(params[:format])
     else
       @user = current_user
