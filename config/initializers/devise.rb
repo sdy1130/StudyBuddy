6da71cb710859f98ba974b685ae609f37f7576de
yui-wong <yui.wong@mail.utoronto.ca>
2019-11-27 01:43:13 -0500

    Changed sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'app152942034@heroku.com'
+  config.mailer_sender = 'https://studybuddy101.herokuapp.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
