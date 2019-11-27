6c5ce73404967f8055b207addab87a59f54405b3
yui-wong <yui.wong@mail.utoronto.ca>
2019-11-27 11:44:19 -0500

    Modified development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,15 +62,14 @@ Rails.application.configure do
 
   # To set up email stuff for user sign up, I based it on this:
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  ActionMailer::Base.delivery_method = :smtp
+
   ActionMailer::Base.smtp_settings = {
     :address              =>  'smtp.sendgrid.net',
-    :port                 =>  '587',
+    :port                 =>  587,
     :authentication       =>  :plain,
     :user_name            =>  ENV['SENDGRID_USERNAME'],
     :password             =>  ENV['SENDGRID_PASSWORD'],
