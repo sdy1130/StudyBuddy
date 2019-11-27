646ba88dd743e29aaeeb3b18efc209032e21f030
yui-wong <yui.wong@mail.utoronto.ca>
2019-11-27 01:16:44 -0500

    Modified enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,17 +90,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'https://studybuddy101.herokuapp.com/' }
-  config.action_mailer.default_charset = "utf-8"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :domain => 'heroku.com',
-    :user_name =>  'app152942034@heroku.com',
-    :password => 'hu80o1ls6768',
-    :authentication => 'plain',
-    :enable_starttls_auto => true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              =>  'smtp.sendgrid.net',
+    :port                 =>  '587',
+    :authentication       =>  :plain,
+    :user_name            =>  'app152942034@heroku.com',
+    :password             =>  'hu80o1ls6768',
+    :domain               =>  'heroku.com',
+    :enable_starttls_auto  =>  true
   }
 
   # Inserts middleware to perform automatic connection switching.
