d8dae98aacfefff660203e637198ea040f5df10e
yui-wong <yui.wong@mail.utoronto.ca>
2019-11-27 00:58:45 -0500

    Modified env for mailer again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,21 +61,21 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # To set up email stuff for user sign up, I based it on this:
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_charset = "utf-8"
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address              =>  'smtp.sendgrid.net',
-    :port                 =>  '587',
-    :authentication       =>  :plain,
-    :user_name            =>  'app152942034@heroku.com',
-    :password             =>  'hu80o1ls6768',
-    :domain               =>  'heroku.com',
-    :enable_starttls_auto  =>  true
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :domain => 'heroku.com',
+    :user_name =>  'app152942034@heroku.com',
+    :password => 'hu80o1ls6768',
+    :authentication => 'plain',
+    :enable_starttls_auto => true
   }
+
 
 end
