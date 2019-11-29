69696094ef5e0daef03d2a7d5e0eb0faa474e98a
yui-wong <yui.wong@mail.utoronto.ca>
2019-11-28 23:42:11 -0500

    Modified env for mailer again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,21 +88,23 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://studybuddy101.herokuapp.com/' }
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'http://study-buddy1011.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address              =>  'smtp.sendgrid.net',
-    :port                 =>  '587',
-    :authentication       =>  :plain,
-    :user_name            =>  ENV['SENDGRID_USERNAME'],
-    :password             =>  ENV['SENDGRID_PASSWORD'],
-    :domain               =>  'heroku.com',
-    :enable_starttls_auto  =>  true
-  }
+  :address              =>  'smtp.sendgrid.net',
+  :port                 =>  '587',
+  :authentication       =>  :plain,
+  :user_name            =>   ENV["SENDGRID_USERNAME"],
+  :password             =>   ENV["SENDGRID_PASSWORD"],
+  :domain               =>  'study-buddy1011.heroku.com',
+  :enable_starttls_auto  =>  true
+}
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
